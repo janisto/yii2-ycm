e803490ebe4de8152bd89012ca2d7ee5cbbb5353
Jani Mikkonen <jani@mikkonen.info>
2015-03-23 08:52:12 +0200

    Update default view

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -9,27 +9,14 @@ $module = Yii::$app->controller->module;
 
 ?>
 <div class="ycm-default-index">
-    <h1>Administration</h1>
-    <?php
-    /*
-    <p>
-        <?= Html::a('Models', ['model/index'], ['class' => 'btn btn-primary']) ?>
-    </p>
-    */ ?>
-
-    <h2>Models</h2>
+    <h1>Models</h1>
 
     <?php foreach ($module->models as $name => $class): ?>
         <h3><?= $module->getAdminName($name) ?></h3>
 
-        <?= Html::a('List ' . $module->getPluralName($name), ['list', 'name' => $name], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('List ' . $module->getPluralName($name), ['model/list', 'name' => $name], ['class' => 'btn btn-primary']) ?>
 
-        <?= Html::a('Create ' . $module->getSingularName($name), ['create', 'name' => $name], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create ' . $module->getSingularName($name), ['model/create', 'name' => $name], ['class' => 'btn btn-success']) ?>
     <?php endforeach; ?>
 
-    <!--
-    <p>
-        <?php echo Yii::t('ycm', 'Test'); ?>
-    </p>
-    -->
 </div>

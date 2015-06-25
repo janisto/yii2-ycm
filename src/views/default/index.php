bacc38f99962e11def13639c8b4dc477451e6c0f
Jani Mikkonen <jani@mikkonen.info>
2015-06-25 16:31:31 +0300

    Add download dropdowns

--- src/views/default/index.php
+++ src/views/default/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use yii\bootstrap\ButtonDropdown;
 use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
@@ -15,6 +16,32 @@ $this->title = Yii::t('ycm', 'Content');
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?php foreach ($module->models as $name => $class): ?>
+        <?php
+        $download = false;
+        $downloadItems = [];
+        if ($module->getDownloadCsv($name)) {
+            $download = true;
+            array_push($downloadItems, [
+                'label' => Yii::t('ycm', 'CSV'),
+                'url' => ['download/csv', 'name' => $name],
+            ]);
+        }
+        if ($module->getDownloadMsCsv($name)) {
+            $download = true;
+            array_push($downloadItems, [
+                'label' => Yii::t('ycm', 'MS CSV'),
+                'url' => ['download/mscsv', 'name' => $name],
+            ]);
+        }
+        if ($module->getDownloadExcel($name)) {
+            $download = true;
+            array_push($downloadItems, [
+                'label' => Yii::t('ycm', 'Excel'),
+                'url' => ['download/excel', 'name' => $name],
+            ]);
+        }
+        ?>
+
         <h3><?= $module->getAdminName($name) ?></h3>
 
         <?= Html::a(Yii::t('ycm', 'List {name}', ['name' => $module->getPluralName($name)]), ['model/list', 'name' => $name], ['class' => 'btn btn-primary']) ?>
@@ -22,6 +49,21 @@ $this->title = Yii::t('ycm', 'Content');
         <?php
         if ($module->getHideCreate($name) === false) {
             echo Html::a(Yii::t('ycm', 'Create {name}', ['name' => $module->getSingularName($name)]), ['model/create', 'name' => $name], ['class' => 'btn btn-success']);
+        }
+        ?>
+
+        <?php
+        if ($download === true) {
+            echo ButtonDropdown::widget([
+                'split' => true,
+                'label' => Yii::t('ycm', 'Download {name}', ['name' => $module->getPluralName($name)]),
+                'dropdown' => [
+                    'items' => $downloadItems,
+                ],
+                'options' => [
+                    'class' => 'btn btn-default',
+                ]
+            ]);
         }
         ?>
 
--- src/views/model/index.php
+++ src/views/model/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use yii\bootstrap\ButtonDropdown;
 use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
@@ -16,6 +17,32 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?php foreach ($module->models as $name => $class): ?>
+        <?php
+        $download = false;
+        $downloadItems = [];
+        if ($module->getDownloadCsv($name)) {
+            $download = true;
+            array_push($downloadItems, [
+                'label' => Yii::t('ycm', 'CSV'),
+                'url' => ['download/csv', 'name' => $name],
+            ]);
+        }
+        if ($module->getDownloadMsCsv($name)) {
+            $download = true;
+            array_push($downloadItems, [
+                'label' => Yii::t('ycm', 'MS CSV'),
+                'url' => ['download/mscsv', 'name' => $name],
+            ]);
+        }
+        if ($module->getDownloadExcel($name)) {
+            $download = true;
+            array_push($downloadItems, [
+                'label' => Yii::t('ycm', 'Excel'),
+                'url' => ['download/excel', 'name' => $name],
+            ]);
+        }
+        ?>
+
         <h3><?= $module->getAdminName($name) ?></h3>
 
         <?= Html::a(Yii::t('ycm', 'List {name}', ['name' => $module->getPluralName($name)]), ['list', 'name' => $name], ['class' => 'btn btn-primary']) ?>
@@ -23,6 +50,21 @@ $this->params['breadcrumbs'][] = $this->title;
         <?php
         if ($module->getHideCreate($name) === false) {
             echo Html::a(Yii::t('ycm', 'Create {name}', ['name' => $module->getSingularName($name)]), ['create', 'name' => $name], ['class' => 'btn btn-success']);
+        }
+        ?>
+
+        <?php
+        if ($download === true) {
+            echo ButtonDropdown::widget([
+                'split' => true,
+                'label' => Yii::t('ycm', 'Download {name}', ['name' => $module->getPluralName($name)]),
+                'dropdown' => [
+                    'items' => $downloadItems,
+                ],
+                'options' => [
+                    'class' => 'btn btn-default',
+                ]
+            ]);
         }
         ?>
 
